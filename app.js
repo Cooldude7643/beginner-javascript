e1fa8af13a4e3d3e189cb9eef2895ea7bc3fa629
cooldude7643 <naturewilderness80845@gmail.com>
2017-06-19 14:58:22 -1000

    Finished

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function fortuneTeller(){
-	console.log("Hello");
+	var numOfChildren = 3
+	var partnersName = "APerson";
+	var geographicalLocation= "Mililani"
+	var Jobtitle = "engineer"
+
+	console.log( "You will be a X in Y, and married to Z with N kids.");
+
+	console.log("You will be a " + Jobtitle + " in " + geographicalLocation + " , and married to " + partnersName + " with " + numOfChildren + " kids ");
 }
+fortuneTeller()
 
 function ageCalculator(){
 
